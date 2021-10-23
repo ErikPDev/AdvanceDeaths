61b6ef6821fcf3f847cb5433cfd3276f78701ca8
ErikPDev <33035080+ErikPDev@users.noreply.github.com>
2021-10-23 09:35:49 +0300

    Bumped Version

--- src/ErikPDev/AdvanceDeaths/Main.php
+++ src/ErikPDev/AdvanceDeaths/Main.php
@@ -87,7 +87,7 @@ class Main extends PluginBase implements Listener {
 
       $this->advanceDeathsCommand = new advancedeaths($this, $this->database);
       $this->isUpdated = true;
-      Server::getInstance()->getAsyncPool()->submitTask(new Update("AdvanceDeaths", "2.7.1"));
+      Server::getInstance()->getAsyncPool()->submitTask(new Update("AdvanceDeaths", "2.8"));
       
       // Discord Webhook
       if($this->getConfig()->get("DiscordEnabled") == true){
