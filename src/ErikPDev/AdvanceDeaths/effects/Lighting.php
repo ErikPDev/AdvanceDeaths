6eb1469e9a194becbb3196e347e3c61bdbd16d50
IvanCraft623 <57236932+IvanCraft623@users.noreply.github.com>
2022-03-19 10:12:02 -0500

    Use packet create static function

--- src/ErikPDev/AdvanceDeaths/effects/Lighting.php
+++ src/ErikPDev/AdvanceDeaths/effects/Lighting.php
@@ -6,7 +6,6 @@ use pocketmine\network\mcpe\protocol\AddActorPacket;
 use pocketmine\network\mcpe\protocol\PlaySoundPacket;
 use pocketmine\Server;
 
-use pocketmine\math\Vector3;
 class Lighting{
     private $player;
     function __construct($player){
@@ -15,22 +14,29 @@ class Lighting{
     
     public function run(){
         $player = $this->player;
-        $light = new AddActorPacket();
-        $light->type = "minecraft:lightning_bolt";
-        $light->entityRuntimeId = Entity::nextRuntimeId();
-        $light->metadata = [];
-        $light->motion = null;
-        $light->yaw = $player->getYaw();
-        $light->pitch = $player->getPitch();
-        $light->position = new Vector3($player->getX(), $player->getY(), $player->getZ());
-        Server::getInstance()->broadcastPacket($player->getWorld()->getPlayers(), $light);
-        $sound = new PlaySoundPacket();
-        $sound->soundName = "ambient.weather.thunder";
-        $sound->x = $player->getX();
-        $sound->y = $player->getY();
-        $sound->z = $player->getZ();
-        $sound->volume = 1;
-        $sound->pitch = 1;
-        Server::getInstance()->broadcastPacket($player->getWorld()->getPlayers(), $sound);
+        $location = $player->getLocation();
+        $id = Entity::nextRuntimeId();
+        $light = AddActorPacket::create(
+            $id, //Actor unique ID not implemented
+            $id,
+            "minecraft:lightning_bolt",
+            $location->asVector3(),
+            null,
+            $location->pitch,
+            $location->yaw,
+            $location->yaw, //Head yaw not implemented
+            [],
+            [],
+            []
+        );
+        $sound = PlaySoundPacket::create(
+            "ambient.weather.thunder",
+            $location->x,
+            $location->y,
+            $location->z,
+            1,
+            1
+        );
+        Server::getInstance()->broadcastPackets($player->getWorld()->getPlayers(), [$light, $sound]);
     }
 }
